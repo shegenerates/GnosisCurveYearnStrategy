d067bdc5b36a5039b2a7cb989ef1337bcd1818c5
Shegen <84243160+shegenerates@users.noreply.github.com>
2022-02-08 18:24:11 -0500

    Initialize cvxCRV-pool

---
+++ contracts/StrategyCurvecvxCRV.sol
@@ -0,0 +1,314 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+// These are the core Yearn libraries
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/Math.sol";
+
+import "./interfaces/curve.sol";
+import "./interfaces/yearn.sol";
+import {IUniswapV2Router02} from "./interfaces/uniswap.sol";
+import {BaseStrategy} from "@yearnvaults/contracts/BaseStrategy.sol";
+
+abstract contract StrategyCurveBase is BaseStrategy {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+
+    /* ========== STATE VARIABLES ========== */
+    // these should stay the same across different wants.
+
+    // curve infrastructure contracts
+    ICurveStrategyProxy public proxy =
+        ICurveStrategyProxy(0xA420A63BbEFfbda3B147d0585F1852C358e2C152); // Yearn's Updated v4 StrategyProxy
+    ICurveFi public curve; // Curve Pool, need this for depositing into our curve pool
+    address public gauge; // Curve gauge contract, most are tokenized, held by Yearn's voter
+
+    // keepCRV stuff
+    uint256 public keepCRV; // the percentage of CRV we re-lock for boost (in basis points)
+    uint256 public constant FEE_DENOMINATOR = 10000; // this means all of our fee values are in bips
+    address public constant voter = 0xF147b8125d2ef93FB6965Db97D6746952a133934; // Yearn's veCRV voter
+
+    // swap stuff
+    address public constant sushiswap =
+        0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F; // default to sushiswap, more CRV liquidity there
+
+    IERC20 public constant crv =
+        IERC20(0xD533a949740bb3306d119CC777fa900bA034cd52);
+    IERC20 public constant weth =
+        IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+
+    bool internal forceHarvestTriggerOnce; // only set this to true externally when we want to trigger our keepers to harvest for us
+
+    string internal stratName; // set our strategy name here
+
+    /* ========== CONSTRUCTOR ========== */
+
+    constructor(address _vault) public BaseStrategy(_vault) {}
+
+    /* ========== VIEWS ========== */
+
+    function name() external view override returns (string memory) {
+        return stratName;
+    }
+
+    function stakedBalance() public view returns (uint256) {
+        return proxy.balanceOf(gauge);
+    }
+
+    function balanceOfWant() public view returns (uint256) {
+        return want.balanceOf(address(this));
+    }
+
+    function estimatedTotalAssets() public view override returns (uint256) {
+        return balanceOfWant().add(stakedBalance());
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+    // these should stay the same across different wants.
+
+    function adjustPosition(uint256 _debtOutstanding) internal override {
+        if (emergencyExit) {
+            return;
+        }
+        // Send all of our LP tokens to the proxy and deposit to the gauge if we have any
+        uint256 _toInvest = balanceOfWant();
+        if (_toInvest > 0) {
+            want.safeTransfer(address(proxy), _toInvest);
+            proxy.deposit(gauge, address(want));
+        }
+    }
+
+    function liquidatePosition(uint256 _amountNeeded)
+        internal
+        override
+        returns (uint256 _liquidatedAmount, uint256 _loss)
+    {
+        uint256 _wantBal = balanceOfWant();
+        if (_amountNeeded > _wantBal) {
+            // check if we have enough free funds to cover the withdrawal
+            uint256 _stakedBal = stakedBalance();
+            if (_stakedBal > 0) {
+                proxy.withdraw(
+                    gauge,
+                    address(want),
+                    Math.min(_stakedBal, _amountNeeded.sub(_wantBal))
+                );
+            }
+            uint256 _withdrawnBal = balanceOfWant();
+            _liquidatedAmount = Math.min(_amountNeeded, _withdrawnBal);
+            _loss = _amountNeeded.sub(_liquidatedAmount);
+        } else {
+            // we have enough balance to cover the liquidation available
+            return (_amountNeeded, 0);
+        }
+    }
+
+    // fire sale, get rid of it all!
+    function liquidateAllPositions() internal override returns (uint256) {
+        uint256 _stakedBal = stakedBalance();
+        if (_stakedBal > 0) {
+            // don't bother withdrawing zero
+            proxy.withdraw(gauge, address(want), _stakedBal);
+        }
+        return balanceOfWant();
+    }
+
+    function prepareMigration(address _newStrategy) internal override {
+        uint256 _stakedBal = stakedBalance();
+        if (_stakedBal > 0) {
+            proxy.withdraw(gauge, address(want), _stakedBal);
+        }
+    }
+
+    function protectedTokens()
+        internal
+        view
+        override
+        returns (address[] memory)
+    {}
+
+    /* ========== KEEP3RS ========== */
+
+    function harvestTrigger(uint256 callCostinEth)
+        public
+        view
+        override
+        returns (bool)
+    {
+        // trigger if we want to manually harvest
+        if (forceHarvestTriggerOnce) {
+            return true;
+        }
+
+        // Should not trigger if strategy is not active (no assets and no debtRatio). This means we don't need to adjust keeper job.
+        if (!isActive()) {
+            return false;
+        }
+
+        return super.harvestTrigger(callCostinEth);
+    }
+
+    /* ========== SETTERS ========== */
+
+    // These functions are useful for setting parameters of the strategy that may need to be adjusted.
+
+    // Use to update Yearn's StrategyProxy contract as needed in case of upgrades.
+    function setProxy(address _proxy) external onlyGovernance {
+        proxy = ICurveStrategyProxy(_proxy);
+    }
+
+    // Set the amount of CRV to be locked in Yearn's veCRV voter from each harvest. Default is 10%.
+    function setKeepCRV(uint256 _keepCRV) external onlyAuthorized {
+        require(_keepCRV <= 10_000);
+        keepCRV = _keepCRV;
+    }
+
+    // This allows us to manually harvest with our keeper as needed
+    function setForceHarvestTriggerOnce(bool _forceHarvestTriggerOnce)
+        external
+        onlyAuthorized
+    {
+        forceHarvestTriggerOnce = _forceHarvestTriggerOnce;
+    }
+}
+
+contract StrategyCurvecvxCRV is StrategyCurveBase {
+    /* ========== STATE VARIABLES ========== */
+    // these will likely change across different wants.
+
+    /* ========== CONSTRUCTOR ========== */
+
+    constructor(
+        address _vault,
+        address _curvePool,
+        address _gauge,
+        string memory _name
+    ) public StrategyCurveBase(_vault) {
+        // You can set these parameters on deployment to whatever you want
+        maxReportDelay = 7 days; // 7 days in seconds
+        debtThreshold = 5 * 1e18; // we shouldn't ever have debt, but set a bit of a buffer
+        profitFactor = 10_000; // in this strategy, profitFactor is only used for telling keep3rs when to move funds from vault to strategy
+        healthCheck = 0xDDCea799fF1699e98EDF118e0629A974Df7DF012; // health.ychad.eth
+
+        // these are our standard approvals. want = Curve LP token
+        want.approve(address(proxy), type(uint256).max);
+
+        // set our keepCRV
+        keepCRV = 1000;
+
+        // this is the pool specific to this vault, used for depositing
+        curve = ICurveFi(_curvePool);
+
+        // set our curve gauge contract
+        gauge = address(_gauge);
+
+        // set our strategy's name
+        stratName = _name;
+
+        // these are our approvals and path specific to this contract
+        crv.approve(address(curve), type(uint256).max);
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+    // these will likely change across different wants.
+
+    function prepareReturn(uint256 _debtOutstanding)
+        internal
+        override
+        returns (
+            uint256 _profit,
+            uint256 _loss,
+            uint256 _debtPayment
+        )
+    {
+        // if we have anything in the gauge, then harvest CRV from the gauge
+        uint256 _stakedBal = stakedBalance();
+        if (_stakedBal > 0) {
+            proxy.harvest(gauge);
+            uint256 _crvBalance = crv.balanceOf(address(this));
+            // if we claimed any CRV, then sell it
+            if (_crvBalance > 0) {
+                // keep some of our CRV to increase our boost
+                uint256 _sendToVoter =
+                    _crvBalance.mul(keepCRV).div(FEE_DENOMINATOR);
+                if (keepCRV > 0) {
+                    crv.safeTransfer(voter, _sendToVoter);
+                }
+                uint256 _crvRemainder = _crvBalance.sub(_sendToVoter);
+
+                // deposit our remaining CRV to Curve
+                if (_crvRemainder > 0) {
+                    curve.add_liquidity([_crvRemainder, 0], 0);
+                }
+            }
+        }
+
+        // debtOustanding will only be > 0 in the event of revoking or if we need to rebalance from a withdrawal or lowering the debtRatio
+        if (_debtOutstanding > 0) {
+            if (_stakedBal > 0) {
+                // don't bother withdrawing if we don't have staked funds
+                proxy.withdraw(
+                    gauge,
+                    address(want),
+                    Math.min(_stakedBal, _debtOutstanding)
+                );
+            }
+            uint256 _withdrawnBal = balanceOfWant();
+            _debtPayment = Math.min(_debtOutstanding, _withdrawnBal);
+        }
+
+        // serious loss should never happen, but if it does (for instance, if Curve is hacked), let's record it accurately
+        uint256 assets = estimatedTotalAssets();
+        uint256 debt = vault.strategies(address(this)).totalDebt;
+
+        // if assets are greater than debt, things are working great!
+        if (assets > debt) {
+            _profit = assets.sub(debt);
+            uint256 _wantBal = balanceOfWant();
+            if (_profit.add(_debtPayment) > _wantBal) {
+                // this should only be hit following donations to strategy
+                liquidateAllPositions();
+            }
+        }
+        // if assets are less than debt, we are in trouble
+        else {
+            _loss = debt.sub(assets);
+        }
+
+        // we're done harvesting, so reset our trigger if we used it
+        forceHarvestTriggerOnce = false;
+    }
+
+    /* ========== KEEP3RS ========== */
+
+    // convert our keeper's eth cost into want
+    function ethToWant(uint256 _ethAmount)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        uint256 callCostInWant;
+        if (_ethAmount > 0) {
+            address[] memory ethPath = new address[](2);
+            ethPath[0] = address(weth);
+            ethPath[1] = address(crv);
+
+            uint256[] memory _callCostInCRVTuple =
+                IUniswapV2Router02(sushiswap).getAmountsOut(
+                    _ethAmount,
+                    ethPath
+                );
+
+            uint256 _callCostInCRV =
+                _callCostInCRVTuple[_callCostInCRVTuple.length - 1];
+            callCostInWant = curve.calc_token_amount([_callCostInCRV, 0], true);
+        }
+        return callCostInWant;
+    }
+}
